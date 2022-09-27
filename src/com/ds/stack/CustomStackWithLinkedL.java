a9ca7134c506f5a85d2a10d470433794fdc775a4
gaurav28384 <gauravkumar.singh@ireslab.com>
2022-09-27 16:22:02 +0530

    CustomStackWithLinkedL : implemented

---
+++ src/com/ds/stack/CustomStackWithLinkedL.java
@@ -0,0 +1,71 @@
+package com.ds.stack;
+
+public class CustomStackWithLinkedL {
+	private StackNode root;
+	int size=0;
+	
+	public static class StackNode{
+		public int data;
+		public StackNode next;
+		
+		public StackNode(int data){
+			this.data = data;
+		}		
+	}
+	
+	public void push(int i){
+		if(root == null){
+			root = new StackNode(i);
+		} else{
+			StackNode temp = root;
+			root = new StackNode(i);
+			root.next = temp;
+		}
+		size++;
+	}
+	
+	public int pop(){
+		if(root == null){
+			System.out.println("Stack is underflow");
+			return 0;
+		} else{
+			int x= root.data;
+			root = root.next;
+			size--;
+			return x;
+		}		
+	}
+	
+	public  int peek(){
+		if(root == null){
+			System.out.println("Stack is underflow");
+			return 0;
+		} else{
+			return root.data;
+		}	
+	}
+	
+	public boolean isEmpty(){
+		return root==null;
+	}
+	
+	public int size(){
+		return size;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		if(root != null){
+			StackNode temp = root;
+			while(temp!=null){
+				sb.append(temp.data+" ");
+				temp = temp.next;
+			}
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+}
--- src/com/ds/stack/StackMain.java
+++ src/com/ds/stack/StackMain.java
@@ -4,6 +4,29 @@ public class StackMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		testCustomStackWithArray();
+		System.out.println("CustomStackWithLinkedL");
+		testCustomStackWithLinkedL();
+	}
+
+	private static void testCustomStackWithLinkedL() {
+		// TODO Auto-generated method stub
+		CustomStackWithLinkedL csLinkedL = new CustomStackWithLinkedL();
+		System.out.println(csLinkedL.pop());
+		System.out.println(csLinkedL.isEmpty());
+		csLinkedL.push(1);
+		csLinkedL.push(9);
+		csLinkedL.push(5);
+		csLinkedL.push(7);
+		System.out.println(csLinkedL.pop());
+		System.out.println(csLinkedL);
+		System.out.println(csLinkedL.isEmpty());
+		System.out.println(csLinkedL.peek());
+		System.out.println(csLinkedL.size());
+	}
+
+	private static void testCustomStackWithArray() {
+		// TODO Auto-generated method stub
 		CustomStackWithArray csArray = new CustomStackWithArray();
 		System.out.println(csArray.pop());
 		System.out.println(csArray.isEmpty());
